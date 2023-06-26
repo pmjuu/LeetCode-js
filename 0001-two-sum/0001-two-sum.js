823f4c7ed09e706307efbced13a7a0189fb36b25
Minju <50537876+pmjuu@users.noreply.github.com>
2023-06-26 11:36:34 +0900

    Time: 2515 ms (5.02%), Space: 57.4 MB (5.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,15 +7,15 @@ const twoSum = function(nums, target) {
     const hashmap = {};
     
     for (let i = 0; i < nums.length; i += 1) {
-        hashmap[nums[i]] = i;
+       hashmap[nums[i]] = i;
     }
     
     for (let i = 0; i < nums.length; i += 1) {
         const rest = target - nums[i];
-        const hashmapKeys = Object.keys(hashmap).map(key => Number(key));
+        const hashmapKeys = Object.keys(hashmap).map(key => Number(key))
         
-        if (hashmapKeys.includes(rest) && hashmap[rest] !== i) {
-            return [i, hashmap[rest]]
+        if (hashmapKeys.includes(rest) && i !== hashmap[rest]) {
+            return [i, hashmap[rest]];
         }
     }
 };
